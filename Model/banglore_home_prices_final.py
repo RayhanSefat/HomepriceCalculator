0e776cb0204c69887ca2a23aed032c2b68bd190d
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-07-01 18:38:38 +0600

    Remove outliers

--- Model/banglore_home_prices_final.py
+++ Model/banglore_home_prices_final.py
@@ -26,7 +26,46 @@ def convert_sqft_to_num(x):
     try:
         return float(x)
     except:
-        return None   
+        return None
+    
+def remove_pps_outliers(df):
+    df_out = pd.DataFrame()
+    for key, subdf in df.groupby('location'):
+        m = np.mean(subdf.price_per_sqft)
+        st = np.std(subdf.price_per_sqft)
+        reduced_df = subdf[(subdf.price_per_sqft>(m-st)) & (subdf.price_per_sqft<=(m+st))]
+        df_out = pd.concat([df_out,reduced_df],ignore_index=True)
+    return df_out
+
+def plot_scatter_chart(df,location):
+    bhk2 = df[(df.location==location) & (df.bhk==2)]
+    bhk3 = df[(df.location==location) & (df.bhk==3)]
+    matplotlib.rcParams['figure.figsize'] = (15,10)
+    plt.scatter(bhk2.total_sqft,bhk2.price,color='blue',label='2 BHK', s=50)
+    plt.scatter(bhk3.total_sqft,bhk3.price,marker='+', color='green',label='3 BHK', s=50)
+    plt.xlabel("Total Square Feet Area")
+    plt.ylabel("Price (Lakh Indian Rupees)")
+    plt.title(location)
+    plt.legend()
+    plt.show()
+
+def remove_bhk_outliers(df):
+    exclude_indices = np.array([])
+    for location, location_df in df.groupby('location'):
+        bhk_stats = {}
+        for bhk, bhk_df in location_df.groupby('bhk'):
+            bhk_stats[bhk] = {
+                'mean': np.mean(bhk_df.price_per_sqft),
+                'std': np.std(bhk_df.price_per_sqft),
+                'count': bhk_df.shape[0]
+            }
+        for bhk, bhk_df in location_df.groupby('bhk'):
+            stats = bhk_stats.get(bhk-1)
+            if stats and stats['count']>5:
+                exclude_indices = np.append(exclude_indices, bhk_df[bhk_df.price_per_sqft<(stats['mean'])].index.values)
+    return df.drop(exclude_indices,axis='index')
+    
+pd.set_option('display.max_columns', None)
 
 df1 = pd.read_csv("bengaluru_house_prices.csv")
 print(df1.head())
@@ -63,3 +102,42 @@ print(location_stats)
 location_stats_less_than_10 = location_stats[location_stats<=10]
 df5.location = df5.location.apply(lambda x: 'other' if x in location_stats_less_than_10 else x)
 print(len(df5.location.unique()))
+
+print(df5[(df5.total_sqft / df5.bhk) < 300].head())
+print(df5.shape)
+
+df6 = df5[-((df5.total_sqft / df5.bhk) < 300)]
+print(df6.shape)
+print(df6.price_per_sqft.describe())
+
+df7 = remove_pps_outliers(df6)
+print(df7.shape)
+    
+# plot_scatter_chart(df7,"Hebbal")
+
+df8 = remove_bhk_outliers(df7)
+print(df8.shape)
+
+# plot_scatter_chart(df8,"Rajaji Nagar")
+
+# matplotlib.rcParams["figure.figsize"] = (20,10)
+# plt.hist(df8.price_per_sqft,rwidth=0.8)
+# plt.xlabel("Price Per Square Feet")
+# plt.ylabel("Count")
+# plt.show()
+
+print(df8.bath.unique())
+print(df8[df8.bath > 10])
+
+# plt.hist(df8.bath,rwidth=0.8)
+# plt.xlabel("Number of bathrooms")
+# plt.ylabel("Count")
+# plt.show()
+
+print(df8[df8.bath > df8.bhk + 2])
+
+df9 = df8[df8.bath < df8.bhk + 2]
+print(df9.shape)
+
+df10 = df9.drop(['size','price_per_sqft'],axis='columns')
+print(df10.head())

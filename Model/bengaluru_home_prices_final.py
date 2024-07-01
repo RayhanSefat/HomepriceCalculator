8beb6e73e37b2b89da9dcb6e7789098f917e4859
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-07-01 21:47:38 +0600

    Finally, build the model

--- Model/bengaluru_home_prices_final.py
+++ Model/bengaluru_home_prices_final.py
@@ -10,6 +10,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
 matplotlib.rcParams["figure.figsize"] = (20,10)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeRegressor
+import pickle
+import json
 
 def is_float(x):
     try:
@@ -64,6 +73,42 @@ def remove_bhk_outliers(df):
             if stats and stats['count']>5:
                 exclude_indices = np.append(exclude_indices, bhk_df[bhk_df.price_per_sqft<(stats['mean'])].index.values)
     return df.drop(exclude_indices,axis='index')
+
+def find_best_model_using_gridsearchcv(X,y):
+    algos = {
+        'linear_regression' : {
+            'model': LinearRegression(),
+            'params': {
+                # 'normalize': [True, False]
+            }
+        },
+        'lasso': {
+            'model': Lasso(),
+            'params': {
+                'alpha': [1,2],
+                'selection': ['random', 'cyclic']
+            }
+        },
+        'decision_tree': {
+            'model': DecisionTreeRegressor(),
+            'params': {
+                'criterion' : ['mse','friedman_mse'],
+                'splitter': ['best','random']
+            }
+        }
+    }
+    scores = []
+    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
+    for algo_name, config in algos.items():
+        gs =  GridSearchCV(config['model'], config['params'], cv=cv, return_train_score=False)
+        gs.fit(X,y)
+        scores.append({
+            'model': algo_name,
+            'best_score': gs.best_score_,
+            'best_params': gs.best_params_
+        })
+
+    return pd.DataFrame(scores,columns=['model','best_score','best_params'])
     
 pd.set_option('display.max_columns', None)
 
@@ -141,3 +186,50 @@ print(df9.shape)
 
 df10 = df9.drop(['size','price_per_sqft'],axis='columns')
 print(df10.head())
+
+dummies = pd.get_dummies(df10.location)
+df11 = pd.concat([df10, dummies.drop(['other'], axis='columns')], axis='columns')
+print(df11.head())
+
+df12 = df11.drop(['location'], axis='columns')
+print(df12.shape)
+X = df12.drop(['price'], axis='columns')
+y = df12.price
+print(X.head())
+print(y.head())
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=10)
+lr_clf = LinearRegression()
+lr_clf.fit(X_train,y_train)
+print(lr_clf.score(X_test,y_test))
+
+cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
+print(cross_val_score(LinearRegression(), X, y, cv=cv))
+
+print(find_best_model_using_gridsearchcv(X,y))
+
+def predict_price(location,sqft,bath,bhk):    
+    loc_index = np.where(X.columns==location)[0][0]
+
+    x = np.zeros(len(X.columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return lr_clf.predict([x])[0]
+
+print(predict_price('1st Phase JP Nagar',1000, 2, 2))
+print(predict_price('1st Phase JP Nagar',1000, 3, 3))
+print(predict_price('Indira Nagar',1000, 2, 2))
+print(predict_price('Indira Nagar',1000, 3, 3))
+
+with open('bengaluru_home_prices_model.pickle','wb') as f:
+    pickle.dump(lr_clf,f)
+
+columns = {
+    'data_columns' : [col.lower() for col in X.columns]
+}
+with open("columns.json","w") as f:
+    f.write(json.dumps(columns))
